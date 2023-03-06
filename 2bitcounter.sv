dbf0e954aabac8e12f93c9d4e22aba5462dd532e
swethac99 <117481228+swethac99@users.noreply.github.com>
2023-03-05 17:09:57 -0800

    updated Global2BitFSM with minor changes

--- 2bitcounter.sv
+++ 2bitcounter.sv
@@ -1,33 +1,31 @@
-module BitSaturatingCounter(
+module Global2BitFSM(
 input        clock,
-input        resetN,
-input        branchIn,
-output logic outcome
+input        reset,
+input        BranchTaken,
+output logic GPreset
 );
+
 localparam NTAKEN=0, TAKEN=1;
+
 typedef enum {SNT, WNT, WT, ST} state;
-// typedef enum  logic {NTAKEN, TAKEN} out;
 
 state currentState, nextState;
-// out outcome;
 
-  assign outcome = (currentState==WT | currentState==ST) ? TAKEN : NTAKEN;
+assign GPresult = (currentState==WT | currentState==ST) ? TAKEN : NTAKEN;
 
-always_ff@(posedge clock)
-if(!resetN)
-currentState <= SNT;
-else
-currentState <= nextState;
+always_ff@(posedge clock or posedge reset)
+  if(!reset)
+    currentState <= SNT;
+  else
+    currentState <= nextState;
 
 always_comb begin
-// outcome = NTAKEN;
-unique case(currentState)
-SNT:  nextState = (branchIn) ? WNT : SNT; 
-WNT:  nextState = (branchIn) ? WT  : SNT; 
-WT :  nextState = (branchIn) ? ST  : WNT; 
-ST :  nextState = (branchIn) ? ST  : WNT; 
-endcase
-
+  unique case(currentState)
+    SNT:  nextState = (BranchTaken) ? WNT : SNT; 
+    WNT:  nextState = (BranchTaken) ? WT  : SNT; 
+    WT :  nextState = (BranchTaken) ? ST  : WNT; 
+    ST :  nextState = (BranchTaken) ? ST  : WNT; 
+  endcase
 end
 
 endmodule
