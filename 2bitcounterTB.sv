e868843a570ec44ea47bac0a7203bb7052a7c88c
Sanket Patil <117404301+sanket1570@users.noreply.github.com>
2023-03-05 17:53:48 -0800

    Created 2 bit global counter TB

--- 2bitcounterTB.sv
+++ 2bitcounterTB.sv
@@ -1,20 +1,54 @@
-module top;
-bit   clock=1;
-logic reset=0;
-logic branchIn;
-logic outcome;
+module global2bitcounterTB;
+  
+    logic clock,reset,BranchTaken,GPresult,GPideal;
+    logic [1:0] count;
+  
+    Global2BitFSM DUT(clock,reset,BranchTaken,GPresult);
+  
+    initial begin
+        clock=0;
+      	forever begin
+	      #2; clock=~clock;
+      	end
+    end
 
-BitSaturatingCounter dut(clock,reset,branchIn,outcome);
+    initial begin
+        RESETFSM(4);
+        repeat(5) update(1);
+	      repeat(5) update(0);
+	      repeat(5) update(1);
+	      repeat(5) update(0);
+	      repeat(2) update(1);
+	      update(0); update(1);update(0);
+	      update(1);update(0);update(1);update(0);
+	      RESETFSM(3);
+	      update(1);
+	      $stop(); 
+	   end
+  
 
-always  clock = #3 ~clock;
+    task RESETFSM(input int t);
+        reset=1'b1;
+      	BranchTaken=1'bx;
+	      count=0;
+  	    repeat(t) begin
+	      GPideal=count<=1 ? 0:1;
+    	  @(negedge clock);
+    	  if(GPresult!==0) $display("Reset ERROR");
+	      end
+    endtask
 
-initial begin
-#5 reset = 1;
-#8 branchIn = 1;
-#5 branchIn = 0;
-#5 branchIn = 1;
+    task update(input logic x);
+        reset=1'b0;
+	      BranchTaken=x;
+	      case(BranchTaken)
+	          0: count = count==0 ? 0 : count-1;
+	          1: count = count==3 ? 3 : count+1;
+	      endcase
+      	GPideal=count<=1 ? 0:1;
+	      @(negedge clock);
+	      if(GPideal !== GPresult) $display("ERROR");
+    endtask
 
-#10 $stop;
-end
 
 endmodule
