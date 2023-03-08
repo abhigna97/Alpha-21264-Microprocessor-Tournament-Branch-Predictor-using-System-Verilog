0c5a9cf9cbd9de0e67e41952309b6d533a2524d0
pdxsasidhar <117481833+pdxsasidhar@users.noreply.github.com>
2023-03-08 10:21:32 -0800

    Create LHT.sv

---
+++ LHT.sv
@@ -0,0 +1,15 @@
+module LHT(clock,reset,BranchTaken,LHresult);
+
+    input logic clock,reset,BranchTaken;
+    output logic LHresult;
+  
+    logic [9:0] LocalHistory='b0;
+  
+    always @ (posedge clock or posedge reset) begin
+        if(reset) LocalHistory<='b0;
+        else LocalHistory<={BranchTaken,LocalHistory[9:1]};
+    end
+  
+    assign LHresult = LocalHistory;
+  
+endmodule
