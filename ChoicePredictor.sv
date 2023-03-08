91f7f4cbd277add6d89ed52c4285266f402b0ec3
swethac99 <117481228+swethac99@users.noreply.github.com>
2023-03-08 13:06:14 -0800

    Update Choice Predictor

--- ChoicePredictor.sv
+++ ChoicePredictor.sv
@@ -2,9 +2,8 @@
 module choice_predictor(
 input clock,
 input reset,
-input ActualLocalPrediction,         //  these are from external source
-input ActualGlobalPrediction,        
-output prediction               // MSB indicates the type of predictor
+input ActualPrediction,         //  these are from external source      
+output prediction               //  MSB indicates the type of predictor
 );
 
 enum {L1, L0, G0, G1} currentState, nextState;
@@ -12,38 +11,34 @@ enum {L1, L0, G0, G1} currentState, nextState;
 assign prediction = (currentState==L0 || currentState==L1) ? 0 : 1; // 0-local, 1-global
 
 always_ff@(posedge clock)
-if(reset)
-currentState <= L0;
-else
-currentState <= nextState;
+    if(reset)
+        currentState <= L0;
+    else
+        currentState <= nextState;
 
 always_comb
-begin
-unique case(currentState)
-L1: if((!ActualGlobalPrediction && ActualLocalPrediction) || (!ActualGlobalPrediction && !ActualLocalPrediction) || (ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = L1;
-else
-    nextState = L0;
+    begin
+        unique case(currentState)
+            L1: if(ActualPrediction)
+                    nextState = L0;
+                else
+                    nextState = L1;
 
-L0: if((!ActualGlobalPrediction && !ActualLocalPrediction) || (ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = L0;
-else if((!ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = L1;
-else
-    nextState = G0;
+            L0: if(ActualPrediction)
+                    nextState = G0;
+                else
+                    nextState = L1;
 
-G0: if((!ActualGlobalPrediction && !ActualLocalPrediction) || (ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = G0;
-else if((!ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = L0;
-else
-    nextState = G1;
+            G0: if(ActualPrediction)
+                    nextState = G1;
+                else
+                    nextState = L0;
 
-G1: if((ActualGlobalPrediction && !ActualLocalPrediction) || (!ActualGlobalPrediction && !ActualLocalPrediction) || (ActualGlobalPrediction && ActualLocalPrediction))
-    nextState = G1;
-else
-    nextState = G0;
-endcase
-end
+            G1: if(ActualPrediction)
+                    nextState = G1;
+                else
+                    nextState = G0;
+        endcase
+    end
 
 endmodule
