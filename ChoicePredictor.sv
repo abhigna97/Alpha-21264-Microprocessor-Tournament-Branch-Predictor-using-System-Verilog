81d3eeaf20e09727eb5e20b5fef2dff7633e617c
swethac99 <117481228+swethac99@users.noreply.github.com>
2023-03-08 14:43:57 -0800

    Changed from FSM to Simple Counter

--- ChoicePredictor.sv
+++ ChoicePredictor.sv
@@ -2,43 +2,19 @@
 module choice_predictor(
 input clock,
 input reset,
-input ActualPrediction,         //  these are from external source      
-output prediction               //  MSB indicates the type of predictor
+input [11:0] PathHistory,
+input ActualPrediction,               
+output prediction               
 );
 
-enum {L1, L0, G0, G1} currentState, nextState;
-
-assign prediction = (currentState==L0 || currentState==L1) ? 0 : 1; // 0-local, 1-global
-
+logic [1:0] CP [4095:0];
+    
+assign prediction = (CP[PathHistory] > 1) ? 1 : 0;
+    
 always_ff@(posedge clock)
     if(reset)
-        currentState <= L1;
+      CP <= '{default:'0};
     else
-        currentState <= nextState;
-
-always_comb
-    begin
-        unique case(currentState)
-            L1: if(ActualPrediction)
-                    nextState = L0;
-                else
-                    nextState = L1;
-
-            L0: if(ActualPrediction)
-                    nextState = G0;
-                else
-                    nextState = L1;
-
-            G0: if(ActualPrediction)
-                    nextState = G1;
-                else
-                    nextState = L0;
-
-            G1: if(ActualPrediction)
-                    nextState = G1;
-                else
-                    nextState = G0;
-        endcase
-    end
+        CP[PathHistory]<= (ActualPrediction) ? CP[PathHistory] + 1'b1 : CP[PathHistory] - 1'b1;
 
 endmodule
