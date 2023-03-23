35b69fe372313d5a3064558fa230c2f33374b6a1
pdxsasidhar <117481833+pdxsasidhar@users.noreply.github.com>
2023-03-22 17:46:44 -0700

    Update AlphaBranchPredictorTB.sv

--- AlphaBranchPredictorTB.sv
+++ AlphaBranchPredictorTB.sv
@@ -9,9 +9,17 @@ always #2 clock=~clock;
 initial begin
 clock=0;
 resetalpha(4);
-repeat(20) updatealpha(0,1);
+  repeat(20) updatealpha(0,1);
 $stop();
 end
+
+logic [9:0] ILHT [1023:0]= '{default:'0};
+logic [2:0] ILPT [1023:0]= '{default:'0};
+logic [11:0] IPH;
+logic [1:0] IGP [4095:0]= '{default:'0};
+logic [1:0] ICP [4095:0]= '{default:'0};
+logic IdealBranch,IdealLocal,IdealGlobal,IdealChoice;
+logic ig,ip;
 
 task resetalpha(input int x);
 reset=1'b1; BranchTaken = 'bx; PC = 'bx;
@@ -20,11 +28,41 @@ endtask
 
 task updatealpha(input logic [9:0] pc,input logic ab);
 reset=1'b0; BranchTaken = 'bx; PC = pc;
-repeat(4) @(negedge clock);
+@(negedge clock);
+IdealLocal = ILPT[ILHT[PC]] >= 4 ? 1 : 0;
+IdealGlobal = IGP[IPH] >= 2 ? 1 : 0;
+IdealChoice = ICP[IPH] >= 2 ? 1 : 0;
+IdealBranch = IdealChoice ? IdealGlobal: IdealLocal;
+repeat(3) @(negedge clock);
+if(IdealBranch!==PredictedBranch) $display("ERROR");
+
 BranchTaken = ab;
-repeat(4) @(negedge clock);
+@(negedge clock);
+
+ig = BranchTaken==IdealGlobal ? 1 : 0;
+ip = BranchTaken==IdealLocal ? 1 : 0;
+case({ig,ip})
+0: ICP[IPH] = ICP[IPH];
+1: ICP[IPH] = ICP[IPH]>0 ? ICP[IPH]-1 : ICP[IPH];
+2: ICP[IPH] = ICP[IPH]<7 ? ICP[IPH]+1 :ICP[IPH];
+3: ICP[IPH] = ICP[IPH];
+endcase
+
+if(BranchTaken) begin
+ILPT[ILHT[PC]] = ILPT[ILHT[PC]] < 7 ?  ILPT[ILHT[PC]]+1 : 7;
+ILHT[PC]=ILHT[PC]<<1 | 1'b1;
+IGP[IPH] = IGP[IPH] < 3 ? IGP[IPH]+1 : 3;
+IPH=IPH>>1 | 12'h800;
+end
+else begin
+ILPT[ILHT[PC]] = ILPT[ILHT[PC]] > 0 ?  ILPT[ILHT[PC]]-1 : 0;
+ILHT[PC]=ILHT[PC]<<1;
+IGP[IPH] = IGP[IPH] > 0 ? IGP[IPH]-1 : 0;
+IPH=IPH>>1;
+end
+
+repeat(3) @(negedge clock);
 endtask
 
 endmodule
-
 
