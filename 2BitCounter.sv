f412d6e7e09c1efebdb76d83c4e7f1f112599807
swethac99 <117481228+swethac99@users.noreply.github.com>
2023-03-22 21:21:49 -0700

    Created 2BitCounter.sv

---
+++ 2BitCounter.sv
@@ -0,0 +1,44 @@
+// Code your design here
+module counter(input ao, input clock, output prediction);
+logic prediction;
+  logic [1:0] sr='0;
+int count;
+assign prediction = sr[1];
+
+
+always_ff@(posedge clock) begin
+  
+case(count)
+0: begin
+  count<=1;
+end
+    
+1: begin
+  
+if(ao)begin
+if(sr==2'b10)
+sr<=2'b11;
+else if(sr==2'b11)
+sr<=2'b11;
+else
+sr<=sr<<1 | 2'b01;
+end
+
+else begin
+
+if(sr==2'b01)
+sr<=2'b00;
+else if(sr==2'b00)
+sr<=2'b00;
+else
+sr<=sr<<1;
+end
+
+count<=0; 
+end
+endcase
+
+end
+
+  
+endmodule
