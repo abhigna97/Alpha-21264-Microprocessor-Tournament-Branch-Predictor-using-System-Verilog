3a2fa36321e5c6b38f2aad6297870a349632264a
Abhigna Bheemineni <117481489+abhi211297@users.noreply.github.com>
2023-03-22 21:50:55 -0700

    Update AlphaBranchPredictorTB.sv

--- AlphaBranchPredictorTB.sv
+++ AlphaBranchPredictorTB.sv
@@ -2,41 +2,39 @@ module AlphaBranchPredictorTB;
 logic clock,reset,BranchTaken,PredictedBranch;
 logic [9:0] PC;
 
+real tests,WrongPredict = 0.0;
+real PredictPercent;
+
 AlphaBranchPredictor A1(clock,reset,PC,BranchTaken,PredictedBranch);
+bind AlphaBranchPredictor Assertions ASRT(clock,reset,PC,BranchTaken,PredictedBranch,A1.G1.LPresult);
+
+always #2 clock=~clock;
+
 class constraints;
 	rand bit 	reset;
 	rand logic [9:0] PC;
 	rand logic BranchTaken;
 	int w_PC5bit0_7=60,w_PC5bit8_15=20,w_PC5bit16_23=10,w_PC5bit24_31=10,w_BranchTaken1=80,w_BranchTaken0=20;
-	constraint rst{
+	constraint rst{								// Constraint to randomize reset with given weights
 		reset dist {1:=1,0:=99};
 	}
-	constraint PCrepeat4_0{
+	constraint PCrepeat4_0{						// Constraint to maintain upper 5 bits of PC constant, and lower 5 bits are randomized as per given weights
 		reset == 1'b0;
 		PC[9:5] inside {0,31};
 		PC[4:0] dist {[0:7]:= w_PC5bit0_7,[8:15]:=w_PC5bit8_15,[16:23]:=w_PC5bit16_23,[24:31]:=w_PC5bit24_31};
 	}
-	constraint PCrepeat9_5{
+	constraint PCrepeat9_5{						// Constraint to maintain lower 5 bits of PC constant, and upper 5 bits are randomized as per given weights
 		reset == 1'b0;
 		PC[4:0] inside {0,31};
 		PC[9:5] dist {[0:7]:= w_PC5bit0_7,[8:15]:=w_PC5bit8_15,[16:23]:=w_PC5bit16_23,[24:31]:=w_PC5bit24_31};
 	}
-	constraint actualbranch{
+	constraint actualbranch{					// Constraint to randomize Actual Branch Taken as per given weights
 		reset == 1'b0;
 		BranchTaken dist {1:=w_BranchTaken1,0:=w_BranchTaken0};
 	}
-	function void pre_randomize();
-		if($get_initial_random_seed > 0)
-			$display("SEED IS SET BY USER");		// use dafault seed
-		else $display("SEED IS NOT SET BY USER");	// if you set a seed using vsim -sv_seed
-	endfunction
-	function void post_randomize();
-		$display("INPUT SEED:%d",$get_initial_random_seed);
-	endfunction
+
 endclass
 constraints cnstr;
-
-always #2 clock=~clock;
 
 initial begin: weighted_randomization
 cnstr = new();
@@ -50,18 +48,18 @@ cnstr.w_PC5bit16_23 = 20;
 cnstr.w_PC5bit24_31 = 10;
 cnstr.w_BranchTaken1= 90;
 cnstr.w_BranchTaken0= 10;
-RANDOMIZATION_FAILURE:assert(cnstr.randomize());
-//reset = cnstr.reset;
-//PC = cnstr.PC;
-//BranchTaken = cnstr.BranchTaken	
-end
-
-initial begin
 clock=0;
 resetalpha(4);
-  repeat(20) updatealpha(0,1);
+  repeat(1000) begin
+RANDOMIZATION_FAILURE:assert(cnstr.randomize());
+ updatealpha(cnstr.PC,cnstr.BranchTaken);
+end
+PredictPercent= ((tests-WrongPredict)/tests)*100;
+$display("Tests: %d ,Wrong Predictions: %d, Predict Percentage: %f ",tests,WrongPredict,PredictPercent);
 $stop();
 end
+
+
 
 logic [9:0] ILHT [1023:0]= '{default:'0};
 logic [2:0] ILPT [1023:0]= '{default:'0};
@@ -78,6 +76,7 @@ endtask
 
 task updatealpha(input logic [9:0] pc,input logic ab);
 reset=1'b0; BranchTaken = 'bx; PC = pc;
+tests+=1;
 @(negedge clock);
 IdealLocal = ILPT[ILHT[PC]] >= 4 ? 1 : 0;
 IdealGlobal = IGP[IPH] >= 2 ? 1 : 0;
@@ -88,6 +87,8 @@ if(IdealBranch!==PredictedBranch) $display("ERROR output = %b expected = %b",Pre
 
 BranchTaken = ab;
 @(negedge clock);
+
+if(PredictedBranch!=BranchTaken) WrongPredict+=1;
 
 ig = BranchTaken==IdealGlobal ? 1 : 0;
 ip = BranchTaken==IdealLocal ? 1 : 0;
@@ -115,4 +116,5 @@ repeat(3) @(negedge clock);
 endtask
 
 endmodule
+
 
